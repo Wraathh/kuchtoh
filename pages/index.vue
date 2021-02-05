898cc587d974bacdf7fe53126070c87dfd49d7ee
Dhavan Kantharaj <dhavankantharaj@gmail.com>
2021-02-05 13:09:51 +0530

    (feat): Basic website design

--- pages/index.vue
+++ pages/index.vue
@@ -1,30 +1,8 @@
 <template>
 <body>
-  <nav class="navbar navbar-expand-lg navbar-dark bg-primary sticky-top">
-  <a class="navbar-brand" href="#">LOGO</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
+  
 
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Services</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link " href="#">Careers</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link " href="#">FAQ</a>
-      </li>
-    </ul>
-    
-  </div>
-</nav>
-  <div >
+  <div>
   <b-jumbotron id="head" align="center"   text-variant="info" >
     <h1>KuchToh Technologies Pvt. Ltd.</h1><br>
      <p>
@@ -36,78 +14,138 @@
 <div class="row">
        <b-card
        id="card"
-       align="left"  
-        
+       align="left"   
        text-variant="info" 
+      >
+         <h4>JOB ID: D001<br>
+             JOB TYPE: Full time Senior Software Developer<br>
+             LOCATION: Bengaluru
+         </h4>
+        <b-card-text>Looking for an enthusiastic, full stack developer, willing to work in a startup setup.<br><br>
+
+          <b><u>REQUIRED SKILLS:</u></b> Node.js, Express.js, Vue/Nuxt, Postgresql,<br>
+          Progressive Web App, with a minimum of 2 years of relevant experience.<br><br>
+
+          <b><u>PREFERRED SKILLS:</u></b> Google location APIs, location/route based search optimization,<br>
+          web application security, payment gateway API, notification APIs, cloud hosting.<br><br>
+
+          <b><u>SALARY:</u></b> 3L to 3.5L per year<br><br>
+
+          <b>To apply, mail your resume to: kuchtohtechnologies@gmail.com.</b></b-card-text>
+      </b-card>
+      <b-card
+      id="card"
+      align="left"   
+      text-variant="info" 
       >
          <h4>JOB ID: D002<br>
              JOB TYPE: Full time Software Developer<br>
              LOCATION: Bengaluru
          </h4>
-        <b-card-text>Looking for enthusiastic software developer, willing to work in a startup setup with work ranging from software maintenance to client interactions.<br>
+        <b-card-text>Looking for enthusiastic <b>software developer</b>, 
+          willing to work in a startup setup with work ranging from software
+          maintenance to client interactions.<br><br>
 
-Required skills: Node.js, Express.js, Vue/Nuxt, Postgresql,<br>
- Progressive Web App, with a minimum of 2 years of relevant experience.<br>
+          <b><u>REQUIRED SKILLS:</u></b> Node.js, Vue/Nuxt, Postgresql, Progressive Web App,<br>
+          with preliminary relevant experience and good communication skills.<br><br>
 
-Preferred skills: Google location APIs, location/route based search optimization,<br>
-web application security, payment gateway API, notification APIs, cloud hosting.<br>
+          <b><u>PREFERRED SKILLS:</u></b> Graphics Design, Video Editing, Social media and Digital Marketing.<br><br>
 
-Salary: 3L to 3.5L per year
+          <b><u>SALARY:</u></b> 3L to 3.5L per year<br><br>
 
-To apply, mail your resume to: kuchtohtechnologies@gmail.com.</b-card-text>
-      </b-card>
-      <b-card
-      id="card"
-       align="left"  
-         
-       text-variant="info" 
-      >
-         <h4>Job ID: D001<br>
-             Job type: Full time Senior Software Developer<br>
-             Location: Bengaluru
-         </h4>
-        <b-card-text>Looking for an enthusiastic, full stack developer, willing to work in a startup setup.<br>
-
-Required skills: Node.js, Express.js, Vue/Nuxt, Postgresql,<br>
- Progressive Web App, with a minimum of 2 years of relevant experience.<br>
-
-Preferred skills: Google location APIs, location/route based search optimization,<br>
-web application security, payment gateway API, notification APIs, cloud hosting.<br>
-
-Salary: 3L to 3.5L per year
-
-To apply, mail your resume to: kuchtohtechnologies@gmail.com.</b-card-text>
+         <b> To apply, mail your resume to: kuchtohtechnologies@gmail.com.</b></b-card-text>
       </b-card>
 
        <b-card
       id="card"
-       align="left"  
-         
-       text-variant="info" 
+      align="left"   
+      text-variant="info" 
       >
-         <h4>Job ID: D001<br>
-             Job type: Full time Senior Software Developer<br>
-             Location: Bengaluru
+         <h4>JOB ID: I001<br>
+             JOB TYPE: Intern(Software Developer)<br>
+             LOCATION: Bengaluru
          </h4>
-        <b-card-text>Looking for an enthusiastic, full stack developer, willing to work in a startup setup.<br>
+        <b-card-text>Looking for enthusiastic <b>graduates/students in computer science domain,</b>, 
+          willing to work in a startup setup.<br><br>
 
-Required skills: Node.js, Express.js, Vue/Nuxt, Postgresql,<br>
- Progressive Web App, with a minimum of 2 years of relevant experience.<br>
+          <b><u>REQUIRED SKILLS:</u></b> HTML, CSS, Python, Graphics Design and Video Editing,<br>
+          Social media and Digital Marketing.<br><br>
 
-Preferred skills: Google location APIs, location/route based search optimization,<br>
-web application security, payment gateway API, notification APIs, cloud hosting.<br>
+          <b><u>PREFERRED SKILLS:</u></b> Vue/Nuxt, Node.js, Postgresql.<br><br>
 
-Salary: 3L to 3.5L per year
+          <b><u>Perks:</u></b><br>1. Experience Certificate<br>2. Letter of recommendation<br>3. Internship extendable to employement based on performance.<br><br>
 
-To apply, mail your resume to: kuchtohtechnologies@gmail.com.</b-card-text>
+         <b> To apply, mail your resume to: kuchtohtechnologies@gmail.com with the subject-line (Job ID/Start Date/Number of Months)</b></b-card-text>
+      </b-card>
+      <b-card
+      id="card"
+      align="left"   
+      text-variant="info" 
+      >
+         <h4>JOB ID: I002<br>
+             JOB TYPE: Intern(Marketing)<br>
+             LOCATION: Bengaluru
+         </h4>
+        <b-card-text>Looking for enthusiastic <b>Marketing post-graduates/graduates,</b> 
+          willing to work in a startup setup.<br><br>
+
+          <b><u>REQUIREMENT:</u></b> MBA in Marketing and Sales<br><br>
+
+          <b><u>PREFERRED SKILLS:</u></b> <br>1. Basic knowledge of Google Analytics, Mixpanel and Google AdWords.<br>
+          2. Basic understanding of different marketing techniques (e.g Point-of-Purchase Marketing, Internet Marketing, Paid Media Advertising, etc).<br>
+          3. Familiar with email marketing techniques.<br>
+          4. Excellent researching abilities.
+          <br><br>
+
+          <b><u>Perks:</u></b><br>1. Experience Certificate<br>2. Letter of recommendation<br>3. Internship extendable to employement based on performance.<br><br>
+
+         <b> To apply, mail your resume to: kuchtohtechnologies@gmail.com with the subject-line (Job ID/Start Date/Number of Months)</b></b-card-text>
+      </b-card>
+      <b-card
+      id="card"
+      align="left"   
+      text-variant="info" 
+      >
+         <h4>JOB ID: I003<br>
+             JOB TYPE: Intern(Law)<br>
+             LOCATION: Bengaluru
+         </h4>
+        <b-card-text><b><u>QUALIFICATION AND SKILLS:</u></b><br>
+        1. Graduated in Law<br>
+        2. Excellent English (oral and written) is a must<br>
+        3. Must have good MS Office skills, mainly MS Word, MS Excel or equivalent<br>
+        <br>
+
+          <b><u>RESPONSIBILITIES:</u></b><br>
+          1. Legal Research - IP Law, Labour Law, Corporate Law, Employment Law, Medical related Legalities<br>
+          2. Legal Drafting - Contracts, Policies, Medical policies, insurance, indemnity etc<br>
+          3. Legal Writing and Content Creation - Articles, Reports, Interviews.<br>
+          4. Business Development related work
+          <br><br>
+
+          <b><u>Perks:</u></b><br>1. Experience Certificate<br>2. Letter of recommendation<br>3. Internship extendable to employement based on performance.<br><br>
+
+         <b> To apply, mail your resume to: kuchtohtechnologies@gmail.com with the subject-line (Job ID/Start Date/Number of Months)</b></b-card-text>
       </b-card>
     </div>
+    <div class="footer">
+  <p>KuchToh Technologies Pvt. Ltd &#169; 2021 </p>
+</div>
     </body>
 </template>
 <style scoped>
+.footer {
+   padding-bottom: 10px;
+   left: 0;
+   bottom: 0;
+   width: 100%;
+   height: 50px;
+   color: white;
+   text-align: center;
+}
 h1 {
   font-family: Arial, sans-serif;
-  font-size: 80px;
+  font-size: 60px;
   color:rgb(253, 238, 209) ;
 
 }
@@ -143,9 +181,7 @@ body{
   
 }
 #head{
- background-color: black;
- 
-  
+ background-color: black; 
 }
 
 </style>
